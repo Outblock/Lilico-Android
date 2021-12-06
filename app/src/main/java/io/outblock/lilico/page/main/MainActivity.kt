b3b75e6284176c8064dc2226b66b20a0a83519c8
kai <kai.wang.john@gmail.com>
2021-12-06 10:48:16 +0800

    main activity

--- app/src/main/java/io/outblock/lilico/page/main/MainActivity.kt
+++ app/src/main/java/io/outblock/lilico/page/main/MainActivity.kt
@@ -1,11 +1,13 @@
 package io.outblock.lilico.page.main
 
 import android.os.Bundle
+import io.outblock.lilico.R
 import io.outblock.lilico.base.activity.BaseActivity
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
