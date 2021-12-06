514e9260e84f60520898abf7a52ba3646ba453fa
kai <kai.wang.john@gmail.com>
2021-12-06 09:22:08 +0800

    MainActivity

---
+++ app/src/main/java/io/outblock/lilico/base/activity/BaseActivity.kt
@@ -0,0 +1,6 @@
+package io.outblock.lilico.base.activity
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity() {
+}
---
+++ app/src/main/java/io/outblock/lilico/page/main/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.outblock.lilico.page.main
+
+import android.os.Bundle
+import io.outblock.lilico.base.activity.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
