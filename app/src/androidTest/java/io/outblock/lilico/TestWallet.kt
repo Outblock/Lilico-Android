e72aff36d9ab126b75ab480065aea10c8caa0a4f
lmcmz <lmcmze@gmail.com>
2021-12-08 01:25:55 +1100

    Fix wallet core test case

--- app/src/androidTest/java/io/outblock/lilico/TestWallet.kt
+++ app/src/androidTest/java/io/outblock/lilico/TestWallet.kt
@@ -5,9 +5,10 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.nftco.flow.sdk.bytesToHex
 import org.junit.Test
 import org.junit.runner.RunWith
-import wallet.core.jni.CoinType
-import wallet.core.jni.Curve
-import wallet.core.jni.HDWallet
+import org.junit.Assert.assertEquals
+import wallet.core.jni.*
+import io.outblock.wallet.extensions.toHex
+import io.outblock.wallet.extensions.toHexBytes
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -26,38 +27,48 @@ class TestWallet {
         Log.w("method", "testCreateMnemonic()")
         val wallet = HDWallet(128, "")
         Log.w("mnemonic", wallet.mnemonic())
+
+        assertEquals(Mnemonic.isValid(wallet.mnemonic()), true)
     }
 
     @Test
     fun testCreateWalletFromMnemonic() {
         Log.w("method", "testCreateMnemonic()")
         val wallet = HDWallet(MNEMONIC, "")
-        Log.w("Flow address", wallet.getAddressForCoin(CoinType.FLOW))
+        assertEquals(MNEMONIC, wallet.mnemonic())
     }
 
     @Test
     fun testWalletKey() {
         Log.w("method", "testWalletKey()")
         val wallet = HDWallet(MNEMONIC, "")
-        Log.w("Flow public key", wallet.getKeyForCoin(CoinType.FLOW).publicKeyNist256p1.data().bytesToHex())
+        val privateKey = wallet.getDerivedKey(CoinType.FLOW, 0, 0, 0)
+        assertEquals(true, PrivateKey.isValid(privateKey.data(), Curve.NIST256P1))
+        assertEquals(PRIVATE_KEY, privateKey.data().bytesToHex())
+        assertEquals(PUBLIC_KEY, privateKey.publicKeyNist256p1.uncompressed().data().bytesToHex())
     }
 
     @Test
     fun testWalletSign() {
         Log.w("method", "testWalletSign()")
+        val data = "hello schnorr".encodeToByteArray()
+        val hashedData = Hash.sha256(data)
         val wallet = HDWallet(MNEMONIC, "")
-        val sign = wallet.getKeyForCoin(CoinType.FLOW).sign(byteArrayOf(0x00), Curve.SECP256K1)
-        Log.w("sign raw", "$sign")
-        Log.w("sign hex", sign?.bytesToHex().orEmpty())
+        val privateKey = wallet.getDerivedKey(CoinType.FLOW, 0, 0, 0)
+        assertEquals(PRIVATE_KEY, privateKey.data().bytesToHex())
+        val signature = privateKey.sign(hashedData, Curve.NIST256P1)
+        Log.w("signature -> ", signature.toHex())
+        assertEquals( true, privateKey.publicKeyNist256p1.verify(signature, hashedData))
+        assertEquals( true, privateKey.publicKeyNist256p1.verify(SIGNATURE.toHexBytes(), hashedData))
     }
 
 
     companion object {
-        const val MNEMONIC = "pride mutual banner proud coil thunder question soccer stone uniform museum wild"
-        const val PUBLIC_KEY = "0x02af7dbda5c7a708bac1f4da132b23c757aa532fd1afb57aa15145f9289d49b043"
+        const val MNEMONIC = "normal dune pole key case cradle unfold require tornado mercy hospital buyer"
+        const val PUBLIC_KEY = "04dbe5b4b4416ad9158339dd692002ceddab895e11bd87d90ce7e3e745efef28d2ad6e736fe3d57d52213f397a7ba9f0bc8c65620a872aefedbc1ddd74c605cf58"
+        const val PRIVATE_KEY = "638dc9ad0eee91d09249f0fd7c5323a11600e20d5b9105b66b782a96236e74cf"
 
-        // TODO 乱写的
-        const val PRIVATE_KEY = "a2f983853e61b3e27d94b7bf3d7094dd756aead2a813dd5cf738e1da56fa9c17"
+        const val SIGNATURE = "0cd37adf53dc353eeb07321c765d81aedd11f34a6393de31bb15e2c5a07793c96ac54369d71a7e769dced55fc941d2f723538e1b31bf587e7f435e911222068b01"
 
         const val HOST_MAINNET = "access.mainnet.nodes.onflow.org"
         const val HOST_TESTNET = "access.devnet.nodes.onflow.org"
