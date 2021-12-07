6098adcfbe85a8c83e49f197befc1d995dac580f
kai <kai.wang.john@gmail.com>
2021-12-07 16:33:47 +0800

    test wallet

---
+++ app/src/androidTest/java/io/outblock/lilico/TestFlowTransaction.kt
@@ -0,0 +1,128 @@
+package io.outblock.lilico
+
+import android.util.Log
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.nftco.flow.sdk.*
+import com.nftco.flow.sdk.cadence.AddressField
+import com.nftco.flow.sdk.cadence.UFix64NumberField
+import com.nftco.flow.sdk.crypto.Crypto
+import io.outblock.lilico.TestWallet.Companion.HOST_TESTNET
+import io.outblock.lilico.TestWallet.Companion.MNEMONIC
+import io.outblock.lilico.TestWallet.Companion.PRIVATE_KEY
+import org.junit.Test
+import org.junit.runner.RunWith
+import wallet.core.jni.CoinType
+import wallet.core.jni.HDWallet
+
+val payerAccountAddress: FlowAddress = FlowAddress("f8d6e0586b0a20c7")
+
+@RunWith(AndroidJUnit4::class)
+class TestFlowTransaction {
+
+    @Test
+    fun testFlowTransaction() {
+        Log.w("method", "testFlowTransaction()")
+        val wallet = HDWallet(MNEMONIC, "")
+        val accessApi = Flow.newAccessApi(HOST_TESTNET, 9000)
+        // TODO 不知道从哪取
+        val accountAddress = FlowAddress("TODO")
+
+        val account = accessApi.getAccountAtLatestBlock(FlowAddress(wallet.getAddressForCoin(CoinType.FLOW)))!!
+
+        val publicKey = wallet.getKeyForCoin(CoinType.FLOW).publicKeyNist256p1
+
+        // TODO 不知道从哪取
+        val privateKey = Crypto.decodePrivateKey(PRIVATE_KEY)
+
+        // TODO 不知道从哪取
+        val keyIndex = 0
+        // TODO 不知道从哪取
+        val signerIndex = 0
+
+        val payerAccountKey = accessApi.getAccountKey(payerAccountAddress, 0)
+
+        accessApi.sendTransaction(
+            FlowTransaction(
+                script = FlowScript(SCRIPT),
+                arguments = listOf(FlowArgument(UFix64NumberField("10")), FlowArgument(AddressField(accountAddress.stringValue))),
+                referenceBlockId = accessApi.getLatestBlockHeader().id,
+                gasLimit = 1000,
+                proposalKey = FlowTransactionProposalKey(
+                    accountAddress,
+                    payerAccountKey.id,
+                    sequenceNumber = payerAccountKey.sequenceNumber.toLong()
+                ),
+                payerAddress = payerAccountAddress,
+                authorizers = listOf(accountAddress),
+                payloadSignatures = listOf(
+                    FlowTransactionSignature(
+                        address = accountAddress,
+                        signerIndex = signerIndex,
+                        keyIndex = keyIndex,
+                        signature = FlowSignature(privateKey.hex)
+                    )
+                ),
+                envelopeSignatures = listOf(
+                    FlowTransactionSignature(
+                        address = payerAccountAddress,
+                        signerIndex = signerIndex,
+                        keyIndex = keyIndex,
+                        signature = FlowSignature(privateKey.hex)
+                    )
+                ),
+            )
+        )
+
+//        val keyPair = Crypto.generateKeyPair(SignatureAlgorithm.ECDSA_SECP256k1)
+//        accessApi.simpleFlowTransaction(
+//            address = FlowAddress(wallet.getAddressForCoin(CoinType.FLOW)),
+//            signer = Crypto.getSigner(keyPair.private, HashAlgorithm.SHA2_256),
+//            gasLimit = 1000,
+//            keyIndex = 0,
+//            block = {
+//                Log.w("block", "simpleFlowTransaction() block callback")
+//            }
+//        )
+    }
+
+    private fun FlowAccessApi.getAccountKey(address: FlowAddress, keyIndex: Int): FlowAccountKey {
+        return getAccountAtLatestBlock(address)!!.keys[keyIndex]
+    }
+
+    companion object {
+        private const val SCRIPT = """
+import FungibleToken from 0xFUNGIBLE_TOKEN_ADDRESS
+import FlowToken from 0xFLOW_TOKEN_ADDRESS
+
+transaction(amount: UFix64, to: Address) {
+
+    // The Vault resource that holds the tokens that are being transferred
+    let sentVault: @FungibleToken.Vault
+
+    prepare(signer: AuthAccount) {
+
+        // Get a reference to the signer's stored vault
+        let vaultRef = signer.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
+            ?? panic("Could not borrow reference to the owner's Vault!")
+
+        // Withdraw tokens from the signer's stored vault
+        self.sentVault <- vaultRef.withdraw(amount: amount)
+    }
+
+    execute {
+
+        // Get the recipient's public account object
+        let recipient = getAccount(to)
+
+        // Get a reference to the recipient's Receiver
+        let receiverRef = recipient.getCapability(/public/flowTokenReceiver)
+            .borrow<&{FungibleToken.Receiver}>()
+            ?? panic("Could not borrow receiver reference to the recipient's Vault")
+
+        // Deposit the withdrawn tokens in the recipient's receiver
+        receiverRef.deposit(from: <-self.sentVault)
+    }
+}
+        """
+    }
+}
--- app/src/androidTest/java/io/outblock/lilico/TestWallet.kt
+++ app/src/androidTest/java/io/outblock/lilico/TestWallet.kt
@@ -2,8 +2,11 @@ package io.outblock.lilico
 
 import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.nftco.flow.sdk.bytesToHex
 import org.junit.Test
 import org.junit.runner.RunWith
+import wallet.core.jni.CoinType
+import wallet.core.jni.Curve
 import wallet.core.jni.HDWallet
 
 /**
@@ -13,14 +16,51 @@ import wallet.core.jni.HDWallet
  */
 @RunWith(AndroidJUnit4::class)
 class TestWallet {
+
     init {
         System.loadLibrary("TrustWalletCore");
     }
 
     @Test
     fun testCreateMnemonic() {
-        Log.w("TEST START", "testCreateMnemonic()")
+        Log.w("method", "testCreateMnemonic()")
         val wallet = HDWallet(128, "")
-        Log.w("MNEMONIC", wallet.mnemonic())
+        Log.w("mnemonic", wallet.mnemonic())
+    }
+
+    @Test
+    fun testCreateWalletFromMnemonic() {
+        Log.w("method", "testCreateMnemonic()")
+        val wallet = HDWallet(MNEMONIC, "")
+        Log.w("Flow address", wallet.getAddressForCoin(CoinType.FLOW))
+    }
+
+    @Test
+    fun testWalletKey() {
+        Log.w("method", "testWalletKey()")
+        val wallet = HDWallet(MNEMONIC, "")
+        Log.w("Flow public key", wallet.getKeyForCoin(CoinType.FLOW).publicKeyNist256p1.data().bytesToHex())
+    }
+
+    @Test
+    fun testWalletSign() {
+        Log.w("method", "testWalletSign()")
+        val wallet = HDWallet(MNEMONIC, "")
+        val sign = wallet.getKeyForCoin(CoinType.FLOW).sign(byteArrayOf(0x00), Curve.SECP256K1)
+        Log.w("sign raw", "$sign")
+        Log.w("sign hex", sign?.bytesToHex().orEmpty())
+    }
+
+
+    companion object {
+        const val MNEMONIC = "pride mutual banner proud coil thunder question soccer stone uniform museum wild"
+        const val PUBLIC_KEY = "0x02af7dbda5c7a708bac1f4da132b23c757aa532fd1afb57aa15145f9289d49b043"
+
+        // TODO 乱写的
+        const val PRIVATE_KEY = "a2f983853e61b3e27d94b7bf3d7094dd756aead2a813dd5cf738e1da56fa9c17"
+
+        const val HOST_MAINNET = "access.mainnet.nodes.onflow.org"
+        const val HOST_TESTNET = "access.devnet.nodes.onflow.org"
+        const val HOST_CANARYNET = "access.canary.nodes.onflow.org"
     }
 }
