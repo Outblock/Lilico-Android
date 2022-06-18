387a22a5cc5224ee86960319fe716007ee20e2d7
kai <kai.wang.john@gmail.com>
2022-06-18 08:52:39 +0800

    firebase function change to http request

--- app/src/main/java/io/outblock/lilico/network/functions/Functions.kt
+++ app/src/main/java/io/outblock/lilico/network/functions/Functions.kt
@@ -1,13 +1,21 @@
 package io.outblock.lilico.network.functions
 
-import com.google.firebase.functions.FirebaseFunctionsException
 import com.google.firebase.functions.ktx.functions
 import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import io.outblock.lilico.firebase.auth.getFirebaseJwt
+import io.outblock.lilico.manager.app.isTestnet
+import io.outblock.lilico.utils.isDev
 import io.outblock.lilico.utils.logd
 import io.outblock.lilico.utils.loge
 import io.outblock.lilico.utils.logw
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
@@ -15,14 +23,39 @@ private const val TAG = "FirebaseFunctions"
 
 const val FUNCTION_SIGN_AS_PAYER = "signAsPayer"
 
+private val HOST = if (isDev()) "https://us-central1-lilico-dev.cloudfunctions.net/" else "https://us-central1-lilico-334404.cloudfunctions.net/"
+
+
 /**
  * execute firebase function
  */
 suspend fun <T> executeFunction(functionName: String, data: Any? = null): T? {
-    return execute(functionName, data)
+    return executeHttp(functionName, data)
 }
 
 private val functions by lazy { Firebase.functions }
+
+private suspend fun <T> executeHttp(functionName: String, data: Any? = null) = suspendCancellableCoroutine<T?> { continuation ->
+    val client = OkHttpClient()
+    val body = if (data == null) data else (if (data is String) data else Gson().toJson(data))
+    logd(TAG, "$functionName http body:$body")
+
+    val jwt = runBlocking { getFirebaseJwt() }
+
+    val request = Request.Builder().url("$HOST$functionName")
+        .post(body.orEmpty().toRequestBody("application/json; charset=utf-8".toMediaType()))
+        .addHeader("authorization", "Bearer $jwt")
+        .addHeader("Network", getNetWork())
+        .build()
+    val response = client.newCall(request).execute()
+
+    if (response.isSuccessful) {
+        logw(TAG, response.toString())
+        continuation.resume(null)
+        return@suspendCancellableCoroutine
+    }
+    continuation.resume(parseResponse(response.body?.string()))
+}
 
 private suspend fun <T> execute(functionName: String, data: Any? = null) = suspendCoroutine<T?> { continuation ->
     val body = if (data == null) data else (if (data is String) data else Gson().toJson(data))
@@ -36,17 +69,20 @@ private suspend fun <T> execute(functionName: String, data: Any? = null) = suspe
                 return@continueWith
             }
 
-            try {
-                val result = task.result?.data?.toString()
-                val obj = Gson().fromJson<T>(result, object : TypeToken<T>() {}.type)
-                continuation.resume(obj)
-            } catch (e: Exception) {
-                if (e is FirebaseFunctionsException) {
-                    logw(TAG, "code:${e.code}, details:${e.details}")
-                }
-                loge(e)
-
-                continuation.resume(null)
-            }
+            continuation.resume(parseResponse(task.result?.data?.toString()))
         }
+}
+
+private fun <T> parseResponse(data: String?): T? {
+    return try {
+        logd(TAG, "response:$data")
+        return Gson().fromJson<T>(data, object : TypeToken<T>() {}.type)
+    } catch (e: Exception) {
+        loge(e)
+        null
+    }
+}
+
+private fun getNetWork(): String {
+    return if (isTestnet()) "testnet" else "mainnet"
 }
