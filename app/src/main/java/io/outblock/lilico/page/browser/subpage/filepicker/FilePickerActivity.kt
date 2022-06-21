6f32c4f907bb66c8239e92a118c62d7559b50800
kai <kai.wang.john@gmail.com>
2022-06-21 17:37:32 +0800

    browser support file picker

--- app/src/main/java/io/outblock/lilico/page/browser/subpage/filepicker/FilePickerActivity.kt
+++ app/src/main/java/io/outblock/lilico/page/browser/subpage/filepicker/FilePickerActivity.kt
@@ -1,18 +1,24 @@
 package io.outblock.lilico.page.browser.subpage.filepicker
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.webkit.WebChromeClient
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.annotation.CallSuper
+import io.outblock.lilico.R
 import io.outblock.lilico.base.activity.BaseActivity
+import io.outblock.lilico.utils.extensions.res2String
 
 class FilePickerActivity : BaseActivity() {
 
-    private val picker = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
-        onWebviewFilePicked(uri)
-        finish()
+    private val picker by lazy {
+        registerForActivityResult(FilePickerResultContract(webviewFileChooserParams()!!)) { uri: Uri? ->
+            onWebviewFilePicked(uri)
+            finish()
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,8 +32,6 @@ class FilePickerActivity : BaseActivity() {
             return
         }
 
-        listOf(FilePickerActivity::class.java).forEach { it }
-
         picker.launch(param.acceptType())
     }
 
@@ -39,5 +43,36 @@ class FilePickerActivity : BaseActivity() {
         fun launch(context: Context) {
             context.startActivity(Intent(context, FilePickerActivity::class.java))
         }
+    }
+}
+
+private class FilePickerResultContract(private val params: WebChromeClient.FileChooserParams) : ActivityResultContract<String, Uri?>() {
+    @CallSuper
+    override fun createIntent(context: Context, input: String): Intent {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            .addCategory(Intent.CATEGORY_OPENABLE)
+            .setType(input)
+        with(intent) {
+            if (params.acceptTypes.size > 1) {
+                putExtra(Intent.EXTRA_MIME_TYPES, params.acceptTypes)
+            }
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            if (params.mode == WebChromeClient.FileChooserParams.MODE_OPEN_MULTIPLE) {
+                putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            }
+        }
+
+        return Intent.createChooser(intent, R.string.file_picker_title.res2String())
+    }
+
+    override fun getSynchronousResult(
+        context: Context,
+        input: String
+    ): SynchronousResult<Uri?>? {
+        return null
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+        return if (intent == null || resultCode != Activity.RESULT_OK) null else intent.data
     }
 }
