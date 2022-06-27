9e9f5ed92d12c4c1856b6f4c766b11cbfd5e1d4b
kai <kai.wang.john@gmail.com>
2022-06-28 06:29:42 +0800

    support mix qrcode

--- app/src/main/java/io/outblock/lilico/utils/BarcodeUtils.kt
+++ app/src/main/java/io/outblock/lilico/utils/BarcodeUtils.kt
@@ -3,6 +3,7 @@ package io.outblock.lilico.utils
 import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import com.google.zxing.client.android.Intents
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanOptions
 import io.outblock.lilico.page.scan.ScanBarcodeActivity
@@ -20,6 +21,7 @@ fun ActivityResultLauncher<ScanOptions>.launch() {
     launch(ScanOptions().apply {
         setOrientationLocked(true)
         setBeepEnabled(false)
+        addExtra(Intents.Scan.SCAN_TYPE, Intents.Scan.MIXED_SCAN)
         captureActivity = ScanBarcodeActivity::class.java
     })
 }
