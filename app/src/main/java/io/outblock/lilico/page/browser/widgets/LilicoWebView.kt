252c8e03cdf2d50e5256c093f0b27446a2747591
kai <kai.wang.john@gmail.com>
2022-07-04 18:29:12 +0800

    remove custom ua

--- app/src/main/java/io/outblock/lilico/page/browser/widgets/LilicoWebView.kt
+++ app/src/main/java/io/outblock/lilico/page/browser/widgets/LilicoWebView.kt
@@ -30,7 +30,7 @@ class LilicoWebView : WebView {
             webViewClient = WebViewClient()
             webChromeClient = WebChromeClient()
             domStorageEnabled = true
-            userAgentString = USER_AGENT
+//            userAgentString = USER_AGENT
         }
         setWebContentsDebuggingEnabled(BuildConfig.DEBUG)
 
@@ -96,7 +96,7 @@ class LilicoWebView : WebView {
     companion object {
         private val TAG = LilicoWebView::class.java.simpleName
         const val USER_AGENT =
-            "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"
+            "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.65 Mobile Safari/537.36"
     }
 }
 
