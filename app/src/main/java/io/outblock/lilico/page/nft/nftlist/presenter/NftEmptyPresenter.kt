729dddb0a74855b77ed8e97a0e46079f429d33f8
kai <kai.wang.john@gmail.com>
2022-07-22 17:34:42 +0800

    nft empty button click

--- app/src/main/java/io/outblock/lilico/page/nft/nftlist/presenter/NftEmptyPresenter.kt
+++ app/src/main/java/io/outblock/lilico/page/nft/nftlist/presenter/NftEmptyPresenter.kt
@@ -1,9 +1,14 @@
 package io.outblock.lilico.page.nft.nftlist.presenter
 
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import io.outblock.lilico.R
 import io.outblock.lilico.databinding.LayoutNftEmptyBinding
+import io.outblock.lilico.page.main.HomeTab
+import io.outblock.lilico.page.main.MainActivityViewModel
 import io.outblock.lilico.utils.extensions.setVisible
+import io.outblock.lilico.utils.findActivity
 import jp.wasabeef.glide.transformations.BlurTransformation
 
 class NftEmptyPresenter(
@@ -12,7 +17,9 @@ class NftEmptyPresenter(
 
     init {
         with(binding) {
-            getNewButton.setOnClickListener { }
+            getNewButton.setOnClickListener {
+                ViewModelProvider(findActivity(binding.root) as FragmentActivity)[MainActivityViewModel::class.java].changeTab(HomeTab.EXPLORE)
+            }
             Glide.with(backgroundImage).load(R.drawable.bg_empty).transform(BlurTransformation(10, 20)).into(backgroundImage)
         }
     }
