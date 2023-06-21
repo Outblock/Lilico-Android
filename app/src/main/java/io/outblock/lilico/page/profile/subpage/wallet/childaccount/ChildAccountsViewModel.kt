c654ceb919ea28f46af4fe4726a5665cba19337b
John <kai.john@foxmail.com>
2023-06-21 16:42:25 +0800

    child account - unlink

--- app/src/main/java/io/outblock/lilico/page/profile/subpage/wallet/childaccount/ChildAccountsViewModel.kt
+++ app/src/main/java/io/outblock/lilico/page/profile/subpage/wallet/childaccount/ChildAccountsViewModel.kt
@@ -3,11 +3,20 @@ package io.outblock.lilico.page.profile.subpage.wallet.childaccount
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.outblock.lilico.manager.childaccount.ChildAccount
+import io.outblock.lilico.manager.childaccount.ChildAccountList
+import io.outblock.lilico.manager.childaccount.ChildAccountUpdateListenerCallback
+import io.outblock.lilico.manager.transaction.OnTransactionStateChange
+import io.outblock.lilico.manager.transaction.TransactionStateManager
 import io.outblock.lilico.manager.wallet.WalletManager
 
-class ChildAccountsViewModel : ViewModel() {
+class ChildAccountsViewModel : ViewModel(), OnTransactionStateChange, ChildAccountUpdateListenerCallback {
 
     val accountsLiveData = MutableLiveData<List<ChildAccount>>()
+
+    init {
+        TransactionStateManager.addOnTransactionStateChange(this)
+        ChildAccountList.addAccountUpdateListener(this)
+    }
 
     fun load() {
         val childAccountList = WalletManager.childAccountList() ?: return
@@ -19,4 +28,14 @@ class ChildAccountsViewModel : ViewModel() {
         load()
     }
 
+    override fun onTransactionStateChange() {
+        val state = TransactionStateManager.getLastVisibleTransaction() ?: return
+        if (state.isSuccess()) {
+            WalletManager.refreshChildAccount()
+        }
+    }
+
+    override fun onChildAccountUpdate(parentAddress: String, accounts: List<ChildAccount>) {
+        load()
+    }
 }
