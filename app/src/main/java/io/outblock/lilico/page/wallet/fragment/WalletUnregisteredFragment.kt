b9d7b3ce4a6fde840881510aefbe3085041a2c27
jaymengxy <jaymengsniper@gmail.com>
2023-09-05 23:16:03 +0800

    fix: profile extension setting icon

--- app/src/main/java/io/outblock/lilico/page/wallet/fragment/WalletUnregisteredFragment.kt
+++ app/src/main/java/io/outblock/lilico/page/wallet/fragment/WalletUnregisteredFragment.kt
@@ -1,6 +1,5 @@
 package io.outblock.lilico.page.wallet.fragment
 
-import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,7 +13,11 @@ class WalletUnregisteredFragment : Fragment() {
 
     private lateinit var binding: FragmentWalletUnregisteredBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentWalletUnregisteredBinding.inflate(inflater)
         return binding.root
     }
@@ -23,15 +26,6 @@ class WalletUnregisteredFragment : Fragment() {
         with(binding) {
             createButton.setOnClickListener { WalletCreateActivity.launch(requireContext()) }
             importButton.setOnClickListener { WalletRestoreActivity.launch(requireContext()) }
-        }
-        setupBackground()
-    }
-
-    private fun setupBackground() {
-        with(binding.root.background as AnimationDrawable) {
-            setEnterFadeDuration(1250)
-            setExitFadeDuration(2500)
-            start()
         }
     }
 }
